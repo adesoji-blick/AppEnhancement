97091111bc00ede4d7760185cccd12e85c4a1d4c
adesoji <adesoji@blick.ng>
2021-09-03 14:40:48 -0600

    refactored code

--- jenkins_dep.sh
+++ jenkins_dep.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 #set ansible user
+# echo setting ansible user
 # sudo su - ansible
 
 # export environment variables
-export AWS_ACCESS_KEY_ID='xxxxxxxx'
-export AWS_SECRET_ACCESS_KEY='xxxxxxxx'
+echo exporting environment variables
+# export AWS_ACCESS_KEY_ID='xxxxxxxx'
+# export AWS_SECRET_ACCESS_KEY='xxxxxxxx'
 export ANSIBLE_HOSTS=ec2.py
 export EC2_INI_PATH=ec2.ini
 
 # Prepare Ansible dynamic inventory
+echo installing dependencies pip 
 ansible-galaxy collection install amazon.aws
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 python get-pip.py
 
 # Install Boto
+echo install boto
 # pip install boto3
 pip install boto
 
-# Access Mgt
+# permission Mgt
+echo setting dynamic inventory permissions
 chmod 755 inventory/ec2.py
 chmod 755 inventory/ec2.ini
